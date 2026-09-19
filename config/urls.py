7ade69d1ca06075da0c2ac61a0b2cad2ce40c2a3
Clice Romão <clice.bbr@gmail.com>
2026-09-19 13:38:52 -0300

    feat: route study pages under competitions

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("concursos/", include("apps.competitions.urls")),
+    path("concursos/", include("apps.studies.urls")),
     path("", include("apps.core.urls")),
 ]
