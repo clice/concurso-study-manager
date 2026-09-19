c2347c5d00cab4d2b9038aedc1ab85e4f0e7148a
Clice Romão <clice.bbr@gmail.com>
2026-09-19 15:30:35 -0300

    feat: route competition questions

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("concursos/", include("apps.competitions.urls")),
     path("concursos/", include("apps.studies.urls")),
+    path("concursos/", include("apps.questions.urls")),
     path("", include("apps.core.urls")),
 ]
