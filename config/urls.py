b91c8f516d1f45ffee26433dfd5cfc73d8c81081
Clice Romão <clice.bbr@gmail.com>
2026-09-19 10:16:54 -0300

    feat: route competition pages

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("concursos/", include("apps.competitions.urls")),
     path("", include("apps.core.urls")),
 ]
