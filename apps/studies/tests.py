101ecf8de7ae8c5772406b2963250c3f41fab097
Clice Romão <clice.bbr@gmail.com>
2026-09-19 15:14:10 -0300

    test: cover dashboard totals row

--- apps/studies/tests.py
+++ apps/studies/tests.py
@@ -508,4 +508,7 @@ class CompetitionDashboardTests(TestCase):
         self.assertContains(response, "Progresso por disciplina")
         self.assertContains(response, "Taxa de acerto por disciplina")
         self.assertContains(response, "Aulas por semana sugerida")
+        self.assertContains(response, "Total")
+        self.assertContains(response, "33.3%")
+        self.assertContains(response, "73.3%")
         self.assertContains(response, "competition-dashboard-data")
